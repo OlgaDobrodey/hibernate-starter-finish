a9f4651baa4567c1fba1a3c5a9cdcbf59b1a777c
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-04-19 14:07:25 +0300

    first commit hibernate-sarter

---
+++ src/main/java/com/itrex/HibernateRunner.java
@@ -0,0 +1,20 @@
+package com.itrex;
+
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.concurrent.BlockingDeque;
+
+public class HibernateRunner {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+//        DriverManager
+//                .getConnection(
+//                        "db.url",
+//                        "db.username",
+//                        "db.password"
+//                );
+    }
+}
